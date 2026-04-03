e832d63330dfc93573412c82103ad3a74f6ff0c6
일혁 <dnfjddk2@gmail.com>
2026-04-03 17:13:03 +0900

    Refactor `RequestHandler` to replace `DataOutputStream` with `HttpResponse` for cleaner response handling and improve method structure.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,16 +2,13 @@ package webserver;
 
 import db.MemoryUserRepository;
 import http.enums.HttpHeader;
-import http.enums.HttpStatus;
-import http.enums.MimeType;
 import http.request.HttpRequest;
+import http.response.HttpResponse;
 import http.routing.Route;
 import model.User;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -31,7 +28,7 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            DataOutputStream dos = new DataOutputStream(out);
+            HttpResponse httpResponse = new HttpResponse(out);
 
             HttpRequest httpRequest = HttpRequest.from(br);
             if (httpRequest == null) return;
@@ -40,37 +37,35 @@ public class RequestHandler implements Runnable {
             log.log(Level.INFO, () -> String.format("Request: %s %s", httpRequest.getMethod(), url));
 
             if (url.equals(Route.SIGNUP.getPath())) {
-                handleSignup(httpRequest, dos);
+                handleSignup(httpRequest, httpResponse);
             } else if (url.equals(Route.LOGIN.getPath())) {
-                handleLogin(httpRequest, dos);
+                handleLogin(httpRequest, httpResponse);
             } else if (url.equals(Route.USER_LIST.getPath())) {
-                handleLoginList(httpRequest, dos);
+                handleLoginList(httpRequest, httpResponse);
             } else {
-                handleStaticFile(url, dos);
+                handleStaticFile(url, httpResponse);
             }
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
     }
 
-    private void handleLoginList(HttpRequest httpRequest, DataOutputStream dos) throws IOException {
+    private void handleLoginList(HttpRequest httpRequest, HttpResponse httpResponse) {
         if (httpRequest.getCookie().contains("logined=true")) {
-            byte[] body = Files.readAllBytes(Paths.get("./webapp/user/list.html"));
-            response200Header(dos, body.length, MimeType.getContentType(".html"));
-            responseBody(dos, body);
+            httpResponse.forward("/user/list.html");
             return;
         }
-        response302Header(dos, Route.LOGIN_PAGE.getPath(), null);
+        httpResponse.redirect(Route.LOGIN_PAGE.getPath());
     }
 
-    private void handleLogin(HttpRequest httpRequest, DataOutputStream dos) {
+    private void handleLogin(HttpRequest httpRequest, HttpResponse httpResponse) {
         String body = httpRequest.getBody();
         log.log(Level.INFO, () -> String.format("Body: %s", body));
 
         String[] params = body.split("&");
         String[] userIdParam = params[0].split("=");
         if (userIdParam.length < 2) {
-            response302Header(dos, Route.LOGIN_FAILED.getPath(), null);
+            httpResponse.redirect(Route.LOGIN_FAILED.getPath());
             return;
         }
         String userId = userIdParam[1];
@@ -79,23 +74,20 @@ public class RequestHandler implements Runnable {
         boolean isSignedUp = memoryUserRepository.isSignedUp(userId);
         log.log(Level.INFO, () -> String.format("isSignedUp: %s", isSignedUp));
         if (isSignedUp) {
-            response302Header(dos, INDEX, HttpHeader.SET_COOKIE.getKey() + ": logined=true");
+            httpResponse.redirect(INDEX, HttpHeader.SET_COOKIE.getKey() + ": logined=true");
             return;
         }
-        response302Header(dos, Route.LOGIN_FAILED.getPath(), null);
+        httpResponse.redirect(Route.LOGIN_FAILED.getPath());
     }
 
-    private void handleStaticFile(String url, DataOutputStream dos) throws IOException {
+    private void handleStaticFile(String url, HttpResponse httpResponse) {
         if (url.equals("/")) {
             url = INDEX;
         }
-        byte[] body = Files.readAllBytes(Paths.get("./webapp" + url));
-
-        response200Header(dos, body.length, MimeType.getContentType(url));
-        responseBody(dos, body);
+        httpResponse.forward(url);
     }
 
-    private void handleSignup(HttpRequest httpRequest, DataOutputStream dos) {
+    private void handleSignup(HttpRequest httpRequest, HttpResponse httpResponse) {
         String body = httpRequest.getBody();
         log.log(Level.INFO, () -> String.format("Body: %s", body));
 
@@ -103,7 +95,7 @@ public class RequestHandler implements Runnable {
         MemoryUserRepository memoryUserRepository = MemoryUserRepository.getInstance();
         memoryUserRepository.addUser(user);
 
-        response302Header(dos, INDEX, null);
+        httpResponse.redirect(INDEX);
     }
 
     private static User getUser(String query) {
@@ -114,42 +106,6 @@ public class RequestHandler implements Runnable {
         String email = params[3].split("=")[1];
 
         return new User(userId, password, name, email);
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
-        try {
-            HttpStatus httpStatus = HttpStatus.OK;
-            dos.writeBytes("HTTP/1.1 " + httpStatus.getCode() + " " + httpStatus.getMessage() + " \r\n");
-            dos.writeBytes(HttpHeader.CONTENT_TYPE.getKey() + ": " + contentType + "\r\n");
-            dos.writeBytes(HttpHeader.CONTENT_LENGTH.getKey() + ": " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos, String path, String cookie) {
-        try {
-            HttpStatus httpStatus = HttpStatus.FOUND;
-            dos.writeBytes("HTTP/1.1 " + httpStatus.getCode() + " " + httpStatus.getMessage() + " \r\n");
-            dos.writeBytes(HttpHeader.LOCATION.getKey() + ": " + path + "\r\n");
-            if (cookie != null && !cookie.isEmpty()) {
-                dos.writeBytes(cookie + "\r\n");
-            }
-            dos.writeBytes("\r\n");
-            dos.flush();
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
     }
 
 }
