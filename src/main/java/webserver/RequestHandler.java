edf64b0f80c4f14c7947f58d4a77982708e82f40
일혁 <dnfjddk2@gmail.com>
2026-04-03 17:04:45 +0900

    Refactor `RequestHandler` to integrate `HttpRequest` for structured request parsing and streamline handler methods.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,8 +4,8 @@ import db.MemoryUserRepository;
 import http.enums.HttpHeader;
 import http.enums.HttpStatus;
 import http.enums.MimeType;
+import http.request.HttpRequest;
 import http.routing.Route;
-import http.util.IOUtils;
 import model.User;
 
 import java.io.*;
@@ -33,18 +33,18 @@ public class RequestHandler implements Runnable {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            String requestLine = br.readLine();
-            if (requestLine == null) return;
-            log.log(Level.INFO, () -> String.format("Request: %s", requestLine));
+            HttpRequest httpRequest = HttpRequest.from(br);
+            if (httpRequest == null) return;
 
-            String url = requestLine.split(" ")[1];
+            String url = httpRequest.getUrl();
+            log.log(Level.INFO, () -> String.format("Request: %s %s", httpRequest.getMethod(), url));
 
             if (url.equals(Route.SIGNUP.getPath())) {
-                handleSignup(br, dos);
+                handleSignup(httpRequest, dos);
             } else if (url.equals(Route.LOGIN.getPath())) {
-                handleLogin(br, dos);
+                handleLogin(httpRequest, dos);
             } else if (url.equals(Route.USER_LIST.getPath())) {
-                handleLoginList(br, dos);
+                handleLoginList(httpRequest, dos);
             } else {
                 handleStaticFile(url, dos);
             }
@@ -53,16 +53,8 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    private void handleLoginList(BufferedReader br, DataOutputStream dos) throws IOException {
-        boolean logined = false;
-        String line = "";
-        while ((line = br.readLine()) != null && !line.isEmpty()) {
-            if (line.contains(HttpHeader.COOKIE.getKey() + ": logined=true")) {
-                logined = true;
-                break;
-            }
-        }
-        if (logined) {
+    private void handleLoginList(HttpRequest httpRequest, DataOutputStream dos) throws IOException {
+        if (httpRequest.getCookie().contains("logined=true")) {
             byte[] body = Files.readAllBytes(Paths.get("./webapp/user/list.html"));
             response200Header(dos, body.length, MimeType.getContentType(".html"));
             responseBody(dos, body);
@@ -71,9 +63,9 @@ public class RequestHandler implements Runnable {
         response302Header(dos, Route.LOGIN_PAGE.getPath(), null);
     }
 
-    private void handleLogin(BufferedReader br, DataOutputStream dos) throws IOException {
-
-        String body = getRequestBody(br);
+    private void handleLogin(HttpRequest httpRequest, DataOutputStream dos) {
+        String body = httpRequest.getBody();
+        log.log(Level.INFO, () -> String.format("Body: %s", body));
 
         String[] params = body.split("&");
         String[] userIdParam = params[0].split("=");
@@ -103,32 +95,15 @@ public class RequestHandler implements Runnable {
         responseBody(dos, body);
     }
 
-    private void handleSignup(BufferedReader br, DataOutputStream dos) throws IOException {
-//        // 요구사항2
-//        String queryString = url.split("\\?")[1];
-//
-//        User user = getUser(queryString);
-//        MemoryUserRepository memoryUserRepository = MemoryUserRepository.getInstance();
-//        memoryUserRepository.addUser(user);
-//        log.log(Level.INFO, () -> String.format("회원가입 완료: %s", userId));
-//
-//        response302Header(dos, INDEX);
-
-        // 요구사항 3
-        String body = getRequestBody(br);
+    private void handleSignup(HttpRequest httpRequest, DataOutputStream dos) {
+        String body = httpRequest.getBody();
+        log.log(Level.INFO, () -> String.format("Body: %s", body));
 
         User user = getUser(body);
         MemoryUserRepository memoryUserRepository = MemoryUserRepository.getInstance();
         memoryUserRepository.addUser(user);
 
         response302Header(dos, INDEX, null);
-    }
-
-    private static String getRequestBody(BufferedReader br) throws IOException {
-        int contentLength = getContentLength(br);
-        String body = IOUtils.readData(br, contentLength);
-        log.log(Level.INFO, () -> String.format("Body: %s", body));
-        return body;
     }
 
     private static User getUser(String query) {
@@ -139,20 +114,6 @@ public class RequestHandler implements Runnable {
         String email = params[3].split("=")[1];
 
         return new User(userId, password, name, email);
-    }
-
-    private static int getContentLength(BufferedReader br) throws IOException {
-        int contentLength = 0;
-        while (true) {
-            String line = br.readLine();
-            if (line.isEmpty()) {
-                break;
-            }
-            if (line.startsWith(HttpHeader.CONTENT_LENGTH.getKey())) {
-                contentLength = Integer.parseInt(line.split(": ")[1]);
-            }
-        }
-        return contentLength;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
